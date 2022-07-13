1aaa2c1d07ed4c7ee930c092bbbb954632f18324
Sabit Ololade <53153510+ololade97@users.noreply.github.com>
2022-07-13 10:45:00 +0100

    Update KoDAO.sol
    Transaction cost reduced by 125,096 - from 3,403,839 to 3,278,743 when deployed.

--- contracts/KoDAO.sol
+++ contracts/KoDAO.sol
@@ -6,6 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
+//Transaction cost reduced from 340,3839 to 327,8743 when deployed.
+
+error AddressNotElegibleForClaim();
+error PurchaseWillExceedSupply();
+error FailedToSend();
+error IncorrectEthValueSent();
+error IncorrectData();
+
 /**
  * @title KoDAO Passes Contract
  * @dev Extends ERC1155 Token Standard basic implementation
@@ -15,26 +23,57 @@ contract KoDAO is ERC1155Supply, Ownable {
     string public symbol;
     uint256 private constant tokenID = 0;
     uint256 public constant maxSupply = 2600;
-    uint256 public constant maxPurchase = 5;
-    uint256 public constant buyPrice = 0.01 ether;
-    uint256 public reserveMaxAmount = 100;
-
+    // uint256 public constant maxPurchase = 5;
+    uint256 public constant mintPrice = 0.1 ether;
+    uint256 public reserveMaxAmount = 260;
+    address public beneficiary;
+    mapping(address => uint256) public presaled;
 
     constructor(string memory _uri) ERC1155(_uri) ERC1155Supply() {
         name = "KoDAO";
         symbol = "KODAO";
     }
 
-    modifier onlyEOA() {
-        require(msg.sender == tx.origin, "Must use Externally Owned Account (EOA)");
-        _;
+    function totalSupply() public view returns (uint256) {
+        return totalSupply(tokenID);
     }
 
-    function mint(address account, uint256 amount) public {
-        require(amount < maxPurchase, "Max purchase exceeded");
-        require(totalSupply(tokenID) + amount < maxSupply, "Purchase would exceed max supply");
+    function mint(address account, uint256 amount) public payable {
+        // require(amount < maxPurchase, "Max purchase exceeded");
+        //require(totalSupply(tokenID) + amount < maxSupply, "Purchase would exceed max supply");
+        if(totalSupply(tokenID) + amount < maxSupply) revert PurchaseWillExceedSupply();
+        //require(mintPrice * amount == msg.value, "Incorrect ETH value sent");
+        if(mintPrice * amount == msg.value) revert IncorrectEthValueSent();
+        _mint(account, tokenID, amount, "");
+    }
+
+    function claim(address account) public {
+        uint256 amount = presaled[account];
+        //require(amount > 0, "Address not eligible for claim");
+        if(amount > 0) revert AddressNotElegibleForClaim();
 
         _mint(account, tokenID, amount, "");
     }
 
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool sent, bytes memory data) = payable(beneficiary).call{ value: balance }("");
+        //require(sent, "Failed to send Ether");
+        if(sent) revert FailedToSend();
+    }
+
+    function setPresaled(address[] calldata accounts, uint256[] calldata amounts)
+        external
+        onlyOwner
+    {
+        //require(accounts.length == amounts.length, "Incorrect data");
+        if(accounts.length == amounts.length) revert IncorrectData();
+        for (uint256 i = 0; i < accounts.length; i++) {
+            presaled[accounts[i]] = amounts[i];
+        }
+    }
+
+    function setPresaled(address account, uint256 amount) external onlyOwner {
+        presaled[account] = amount;
+    }
 }
